4c63bcf1932579e31d0979950d77cb4dddc3eb28
ruanyl <ruanyu1@gmail.com>
2014-01-14 00:55:52 +0200

    now items can keep their states when add new item to list

--- src/client/grocery.js
+++ src/client/grocery.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var _ = require('mori');
 
+var sid = 0;
+
 var AddGroceryItemInput = React.createClass({
     render: function() {
         var itemNameInput = React.DOM.input({
@@ -29,7 +31,7 @@ var AddGroceryItemInput = React.createClass({
         var itemName = this.refs.name.getDOMNode().value.trim();
         if (itemName) {
             this.refs.name.getDOMNode().value = '';
-            var groceryItem = _.hash_map('name', itemName,
+            var groceryItem = _.hash_map('itemId', sid++, 'name', itemName,
                 'completed', false);
             this.props.onAddItem(groceryItem);
         }
@@ -37,9 +39,13 @@ var AddGroceryItemInput = React.createClass({
 });
 
 var GroceryItem = React.createClass({
+    /*
     getInitialState: function() {
-        return {name: this.props.name, completed: this.props.completed};
-    },
+        return {
+            name: this.props.name,
+            completed: this.props.completed
+        };
+    },*/
     render: function() {
         var checkbox = React.DOM.input({
                 type: 'checkbox',
@@ -54,8 +60,10 @@ var GroceryItem = React.createClass({
             this.props.name);
     },
     handleCompletedClick: function() {
+        var itemId = this.props.itemId;
+        this.props.onCompleteItem(itemId);
         this.props.completed = !this.props.completed;
-        this.setState({name: this.props.name, completed: this.props.completed});
+        //this.setState({name: this.props.name, completed: this.props.completed});
     }
 });
 
@@ -71,13 +79,32 @@ var GroceryList = React.createClass({
         this.setState({
             items: updatedItems
         });
+   },
+    completeItem: function(itemId) {
+        var items = this.state.items;
+        var updatedItems = _.vector();
+            //console.log(itemId);
+        _.each(items, function(item){
+            if(_.get(item, 'itemId') == itemId) {
+                //console.log(_.get(item, 'itemId'));
+                item = _.update_in(item, ['completed'], function() {
+                    return !_.get(item, 'completed');
+                });
+                //console.log(_.get(item, 'completed'));
+            }
+            updatedItems = _.conj(updatedItems, item);
+        });
+        this.setState({items: updatedItems});
     },
     render: function() {
         var items = this.state.items;
+        var completeItemFunc = this.completeItem;
         var itemNodes = _.into_array(_.map(function(item) {
             return GroceryItem({
                 name: _.get(item, 'name'),
-                completed: _.get(item, 'completed')
+                completed: _.get(item, 'completed'),
+                itemId: _.get(item, "itemId"),
+                onCompleteItem: completeItemFunc
             });
         }, items));
 
@@ -94,9 +121,9 @@ var GroceryList = React.createClass({
 
 function render() {
     var initialItems = _.vector(
-        _.hash_map('name', '1 packages of tomato puree', 'completed', false),
-        _.hash_map('name', '4 yellow onions', 'completed', true),
-        _.hash_map('name', '2 dl cream', 'completed', false));
+        _.hash_map('itemId', sid++, 'name', '1 packages of tomato puree', 'completed', false),
+        _.hash_map('itemId', sid++, 'name', '4 yellow onions', 'completed', true),
+        _.hash_map('itemId', sid++, 'name', '2 dl cream', 'completed', false));
 
     var groceryListInitialState = {
         initialItems: initialItems
