67e69248a3adbdf4294377adc731f70c384c2f6d
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2014-01-28 18:58:20 +0200

    Display sign in and sign up buttons, switch between

--- src/client/sign_in.js
+++ src/client/sign_in.js
@@ -3,45 +3,93 @@ var _ = require('mori');
 var Bacon = require('baconjs');
 
 var SignInForm = React.createClass({
+  getEmailForm: function(){
+    return React.DOM.div({className: 'form-group'},
+                         React.DOM.input({className: 'form-control',
+                                          type: 'email',
+                                          ref: 'email',
+                                          placeholder: 'Email address'}));
+  },
+  getPasswordForm: function(){
+    return React.DOM.div({className: 'form-group'},
+                         React.DOM.input({className: 'form-control',
+                                          type: 'password',
+                                          ref: 'password',
+                                          placeholder: 'Password'}));
+  },
+  getConfirmPassForm: function(signingUp){
+    if(signingUp){
+      return React.DOM.div({className: 'form-group'},
+                           React.DOM.input({className: 'form-control',
+                                            type: 'password',
+                                            ref: 'confirmPass',
+                                            placeholder: 'Confirm Password'}));
+    } else {
+      return null;
+    }
+  },
+  getSignUpButton: function(){
+    return React.DOM.button({className: 'btn btn-success',
+                             type: 'button',
+                             onClick: this.onSignUpClick},
+                            "Sign Up");
+  },
+  getSignInButton: function(){
+    return React.DOM.button({className: 'btn btn-primary',
+                             type: 'button',
+                             onClick: this.onSignInClick},
+                            "Sign In");
+  },
+  getCancelButton: function(){
+    return React.DOM.button({className: 'btn btn-default',
+                             type: 'button',
+                             onClick: this.onCancelClick},
+                            "Cancel");
+  },
+  getButtons: function(signingUp){
+    if(signingUp){
+      return React.DOM.div({className: 'pull-right'}, 
+                           this.getCancelButton(), 
+                           this.getSignUpButton());
+    } else {
+      return React.DOM.div({className: 'pull-right'}, 
+                           this.getSignUpButton(), 
+                           this.getSignInButton());
+    }
+  },
+  getInitialState: function(){
+    return {signingUp: false};
+  },
   render: function(){
-    var emailForm = React.DOM.div({className: 'form-group'},
-                                  React.DOM.input({className: 'form-control',
-                                                   type: 'email',
-                                                   ref: 'email',
-                                                   placeholder: 'Email address'}));
-    var passwordForm = React.DOM.div({className: 'form-group'},
-                                     React.DOM.input({className: 'form-control',
-                                                      type: 'password',
-                                                      ref: 'password',
-                                                      placeholder: 'Password'}));
-
-    var confirmPassForm = React.DOM.div({className: 'form-group'},
-                                        React.DOM.input({className: 'form-control',
-                                                         type: 'password',
-                                                         ref: 'confirmPass',
-                                                         placeholder: 'Confirm Password'}));
-
-    var submitButton = React.DOM.button({className: 'btn btn-default',
-                                         type: 'button',
-                                         onClick: this.onSignUpClick},
-                                        "Sign Up");
-
-    return React.DOM.form({className: 'well well-lg'}, 
-                          emailForm,
-                          passwordForm,
-                          confirmPassForm,
-                          submitButton);
+    var signingUp = this.state.signingUp;
+    return React.DOM.form({className: 'sign-in-form well well-lg clearfix'}, 
+                          this.getEmailForm(),
+                          this.getPasswordForm(),
+                          this.getConfirmPassForm(signingUp),
+                          this.getButtons(signingUp));
   },
   onSignUpClick: function(){
-    var email = this.refs.email.getDOMNode().value.trim();
-    var password = this.refs.password.getDOMNode().value.trim();
-    var confirmPass = this.refs.confirmPass.getDOMNode().value.trim();
-    console.log('Sign up email:', email, 'pass:', password, 'confirm:', confirmPass);
+    if(this.state.signingUp){
+      var email = this.refs.email.getDOMNode().value.trim();
+      var password = this.refs.password.getDOMNode().value.trim();
+      var confirmPass = this.refs.confirmPass.getDOMNode().value.trim();
+      console.log('Sign up email:', email, 'pass:', password, 'confirm:', confirmPass);
 
-    var event = _.hash_map('eventType', 'signUp', 
-                           'email', email,
-                           'password', password);
-    outgoingEvents.push(event);
+      var event = _.hash_map('eventType', 'signUp', 
+                             'email', email,
+                             'password', password);
+      outgoingEvents.push(event);
+    } else {
+      this.setState({signingUp: true});
+    }
+  },
+  componentDidUpdate: function(prevProps, prevState, rootNode){
+    if(prevState.signingUp === false && this.state.signingUp === true){
+      this.refs.confirmPass.getDOMNode().focus();
+    }
+  },
+  onCancelClick: function(){
+    this.setState({signingUp: false});
   }
 });
 
