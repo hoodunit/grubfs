9bd5c8ddbfdca5359cb30b6ed5bfe6e2100ed3cc
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2014-02-04 23:42:22 +0200

    Increase size of Twitter bootstrap columns

--- src/client/view/view.js
+++ src/client/view/view.js
@@ -20,12 +20,12 @@ var GrubView = React.createClass({
                         signedIn: signedIn};
 
     return React.DOM.div({}, 
-                         React.DOM.div({className: 'col-sm-2'}),
-                         React.DOM.div({className: 'col-sm-5'},
+                         React.DOM.div({className: 'col-md-2'}),
+                         React.DOM.div({className: 'col-md-5'},
                                        Grocery.GroceryList(groceryState)),
-                         React.DOM.div({className: 'col-sm-3'},
+                         React.DOM.div({className: 'col-md-3'},
                                        this.getSignInForm(signedIn)),
-                         React.DOM.div({className: 'col-sm-2'}));
+                         React.DOM.div({className: 'col-md-2'}));
   }
 });
 
