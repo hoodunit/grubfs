616377dc2b1cafdd5b71d93b0a5adfd7e29ab974
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2014-02-04 14:45:07 +0200

    Grunt: Also watch server, shared source files

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     },
     watch: {
       scripts: {
-        files: ['src/**/*.js'],
+        files: ['src/**/*.js', 'test/**/*.js'],
         tasks: ['jshint', 'browserify', 'mochaTest', 'mocha_phantomjs'],
         options: {
           spawn: false
