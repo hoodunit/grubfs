d88dfdcb24afb844a240392bde08e5b0efa7a83c
Tuure Savuoja <tuure@tuuresavuoja.net>
2014-03-16 13:43:46 +0200

    logged in user name shown next to the logout button

--- src/client/view/view.js
+++ src/client/view/view.js
@@ -4,13 +4,12 @@ var _ = require('mori');
 
 var Grocery = require('./grocery');
 var SignIn = require('./sign_in');
-var UserInfo = require('./user_info');
 var State = require('../state');
 
 var GrubView = React.createClass({
-  getSignInFormOrUserInfo: function(signedIn){
+  getSignInForm: function(signedIn){
     if(signedIn){
-      return UserInfo.UserInfo(_.get(this.props, 'credentials'));
+      return null;
     } else {
       return SignIn.SignInForm({});
     }
@@ -18,14 +17,16 @@ var GrubView = React.createClass({
   render: function() {
     var signedIn = State.signedIn(this.props);
     var groceryState = {items: _.get(this.props, 'items'),
-                        signedIn: signedIn};
+                        signedIn: signedIn,
+                        email:  _.get(_.get(this.props, 'credentials'), 'email')
+    };
 
     return React.DOM.div({}, 
                          React.DOM.div({className: 'col-md-2'}),
                          React.DOM.div({className: 'col-md-5'},
                                        Grocery.GroceryList(groceryState)),
                          React.DOM.div({className: 'col-md-3'},
-                                       this.getSignInFormOrUserInfo(signedIn)),
+                                       this.getSignInForm(signedIn)),
                          React.DOM.div({className: 'col-md-2'}));
   }
 });
